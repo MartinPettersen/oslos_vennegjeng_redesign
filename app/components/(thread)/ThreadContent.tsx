08b7c96203ff98e4db4fdf56a034e95f620a60ec
MartinPettersen <searchingtheskyfor@gmail.com>
2024-03-18 11:41:57 +0100

    add option to report thread

--- app/components/(thread)/ThreadContent.tsx
+++ app/components/(thread)/ThreadContent.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Thread } from "@/types/Thread";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TrashIcon } from "@heroicons/react/20/solid";
 import { PencilIcon } from "@heroicons/react/20/solid";
 import { useSession } from "next-auth/react";
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import EditThread from "./EditThread";
 import ThreadShare from "./ThreadShare";
 import UserNameLink from "./UserNameLink";
+import ReportForm from "./ReportForm";
 
 type Props = {
   thread: Thread;
@@ -17,6 +18,7 @@ const ThreadContent = ({ thread }: Props) => {
   const router = useRouter();
 
   const [toggle, setToggle] = useState(false);
+  const [winReady, setwinReady] = useState(false);
 
   const { data: session }: any = useSession({
     required: true,
@@ -40,6 +42,10 @@ const ThreadContent = ({ thread }: Props) => {
     }
   };
 
+  useEffect(() => {
+    setwinReady(true);
+  }, []);
+
   return (
     <div>
       {toggle ? (
@@ -48,21 +54,28 @@ const ThreadContent = ({ thread }: Props) => {
         <div className="flex flex-col bg-white rounded-xl justify-center gap-4 p-4">
           <div className="flex justify-between items-center text-sky-300 ">
             <h1 className="font-bold text-xl">{thread!.headline}</h1>
-            <div className="">{thread!.replies.length}</div>
-            {session?.user?.name === thread?.userName ? (
-              <div className=" flex gap-2">
-                <PencilIcon
-                  onClick={() => setToggle(!toggle)}
-                  className="h-4 w-4 hover:cursor-pointer"
-                />
-                <TrashIcon
-                  onClick={() => handleDelete()}
-                  className="h-4 w-4 text-red-500 hover:cursor-pointer"
-                />
-              </div>
-            ) : (
-              <></>
-            )}
+            <div className=" flex gap-2">
+              <div className="">{thread!.replies.length}</div>
+              {winReady ? (
+                <ReportForm subjectType="thread" subjectId={thread!.id} />
+              ) : (
+                <></>
+              )}
+              {session?.user?.name === thread?.userName ? (
+                <div className=" flex gap-2">
+                  <PencilIcon
+                    onClick={() => setToggle(!toggle)}
+                    className="h-4 w-4 hover:cursor-pointer"
+                  />
+                  <TrashIcon
+                    onClick={() => handleDelete()}
+                    className="h-4 w-4 text-red-500 hover:cursor-pointer"
+                  />
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
           <UserNameLink userName={thread!.userName} />
           <p className="text-sky-300">{thread!.content}</p>
