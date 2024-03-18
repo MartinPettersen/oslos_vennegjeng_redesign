317485a5c032c43a4b3fef6495da0a9854b21f6b
MartinPettersen <searchingtheskyfor@gmail.com>
2024-03-18 11:19:23 +0100

    add information to repport page

--- app/components/(report)/ReportContainer.tsx
+++ app/components/(report)/ReportContainer.tsx
@@ -7,9 +7,12 @@ type Props = {
 
 const ReportContainer = ({reports}: Props) => {
   return (
-    <div className='bg-white bg-opacity-30 rounded-xl flex items-start sm:items-center justify-center w-[90%] h-[90%]'>
+    <div className='bg-white text-slate-500 bg-opacity-30 rounded-xl flex gap-2 sm:gap-4 flex-col items-center sm:items-center justify-start sm:justify-center w-[90%] h-[90%]'>
         <div className='font-bold text-3xl'>Rapporter</div>
-        <p>{`Det er ${reports?.length}`}</p>
+        <p>{`Det er ${reports?.length} rapporter som venter`}</p>
+        {reports?.map((report => (
+            <div>{report.reason}</div>
+        )))}
     </div>
   )
 }
